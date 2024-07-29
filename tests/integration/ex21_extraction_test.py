a6cf4b17d67864bb8df6d78cf7e74430be2f1953
Katie Lamb <katherine.lamb@catalyst.coop>
2024-07-28 23:28:33 -0700

    actually add the testing

---
+++ tests/integration/ex21_extraction_test.py
@@ -0,0 +1,35 @@
+"""Integration testing for labeling entities with LayoutLM and extracting Ex. 21 tables."""
+
+from mozilla_sec_eia.ex_21.inference import create_inference_dataset, perform_inference
+from mozilla_sec_eia.ex_21.train_extractor import LABELS, load_model
+from transformers import AutoProcessor
+
+
+def test_model_loading():
+    """Test loading a fine-tuned LayoutLM model from MLFlow."""
+    load_model()
+
+
+def test_dataset_creation(test_dir):
+    pdf_dir = test_dir / "data/test_pdfs"
+    dataset = create_inference_dataset(pdfs_dir=pdf_dir)
+    assert dataset.shape == (2, 4)
+
+
+def test_inference_and_table_extraction(test_dir):
+    """Test performing inference and extracting an Ex. 21 table."""
+    model = load_model()
+    processor = AutoProcessor.from_pretrained(
+        "microsoft/layoutlmv3-base", apply_ocr=False
+    )
+    pdf_dir = test_dir / "data/test_pdfs"
+    logit_list, pred_list, output_df = perform_inference(
+        pdfs_dir=pdf_dir,
+        model=model,
+        processor=processor,
+        device="cpu",
+    )
+    assert logit_list[0].shape == (1, 512, len(LABELS))
+    # expected_out_path = test_dir / "inference_and_extraction_expected_out.csv"
+    # expected_out_df = pd.read_csv(expected_out_path)
+    # assert_frame_equal(expected_out_df, output_df)
