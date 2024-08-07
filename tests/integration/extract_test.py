d72fb48c18eaa25bf5f7c6e02d86056ef27d3e81
zschira <zach.schira@catalyst.coop>
2024-08-07 11:13:18 -0400

    Don't use remote tracking server in integration tests

--- tests/integration/extract_test.py
+++ tests/integration/extract_test.py
@@ -1,5 +1,7 @@
 """Validate basic 10k and exhibit 21 extraction."""
 
+import unittest
+
 from mozilla_sec_eia.extract import (
     _get_experiment_name,
     _get_most_recent_run,
@@ -7,9 +9,11 @@ from mozilla_sec_eia.extract import (
 )
 
 
-def test_basic_10k_extraction():
+def test_basic_10k_extraction(test_mlflow_init_func):
     """Run full 10k extraction on validation set and verify desired metrics are met."""
-    validate_extraction("basic_10k")
+    with unittest.mock.patch("mozilla_sec_eia.extract.initialize_mlflow"):
+        test_mlflow_init_func()
+        validate_extraction("basic_10k")
     run = _get_most_recent_run(
         _get_experiment_name("basic_10k", experiment_suffix="validation")
     )
